82299d25a1e106e0a382ce1c5edbc71f32d4d995
bussi <bussidn@gmail.com>
2020-06-23 11:12:29 +0200

    create nested classes on tests to differenciate them

--- src/test/java/dbus/_1/theJoyFulDays/ApplicationTest.java
+++ src/test/java/dbus/_1/theJoyFulDays/ApplicationTest.java
@@ -2,36 +2,41 @@ package dbus._1.theJoyFulDays;
 
 import dbus._1.theJoyFulDays.type.A;
 import dbus._1.theJoyFulDays.type.G;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class ApplicationTest {
 
-    @Test
-    public void application_should_return_G_when_provided_A() {
-        // given
-        Application application = new Application();
-        A a = A.INSTANCE;
+    @Nested
+    public class _1TheJoyfulDays {
 
-        // when
-        G result = application.run(a);
+        @Test
+        public void application_should_return_G_when_provided_A() {
+            // given
+            Application application = new Application();
+            A a = A.INSTANCE;
 
-        // then
-        assertThat(result).isEqualTo(G.INSTANCE);
-    }
+            // when
+            G result = application.run(a);
 
-    @Test
-    public void application_should_return_G_when_provided_A_when_calling_the_more_familiar_version() {
-        // given
-        Application application = new Application();
-        A a = A.INSTANCE;
+            // then
+            assertThat(result).isEqualTo(G.INSTANCE);
+        }
 
-        // when
-        G result = application.moreFamiliarRun(a);
+        @Test
+        public void application_should_return_G_when_provided_A_when_calling_the_more_familiar_version() {
+            // given
+            Application application = new Application();
+            A a = A.INSTANCE;
 
-        // then
-        assertThat(result).isEqualTo(G.INSTANCE);
+            // when
+            G result = application.moreFamiliarRun(a);
+
+            // then
+            assertThat(result).isEqualTo(G.INSTANCE);
+        }
     }
 
 }
--- src/test/java/dbus/_2/firstProductionIssue/ApplicationTest.java
+++ src/test/java/dbus/_2/firstProductionIssue/ApplicationTest.java
@@ -2,36 +2,40 @@ package dbus._2.firstProductionIssue;
 
 import dbus._2.firstProductionIssue.type.A;
 import dbus._2.firstProductionIssue.type.G;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class ApplicationTest {
 
-    @Test
-    public void application_should_return_G_when_provided_A_is_valid() {
-        // given
-        Application application = new Application();
-        A a = A.VALID_INSTANCE;
+    @Nested
+    public class _1FirstProductionIssue {
 
-        // when
-        G result = application.run(a);
+        @Test
+        public void application_should_return_G_when_provided_A_is_valid() {
+            // given
+            Application application = new Application();
+            A a = A.VALID_INSTANCE;
 
-        // then
-        assertThat(result).isEqualTo(G.INSTANCE);
+            // when
+            G result = application.run(a);
+
+            // then
+            assertThat(result).isEqualTo(G.INSTANCE);
+        }
+
+        @Test
+        public void application_should_return_null_when_provided_A_is_not_valid() {
+            // given
+            Application application = new Application();
+            A a = A.NON_VALID_INSTANCE;
+
+            // when
+            G result = application.run(a);
+
+            // then
+            assertThat(result).isNull();
+        }
     }
-
-    @Test
-    public void application_should_return_null_when_provided_A_is_not_valid() {
-        // given
-        Application application = new Application();
-        A a = A.NON_VALID_INSTANCE;
-
-        // when
-        G result = application.run(a);
-
-        // then
-        assertThat(result).isNull();
-    }
-
 }
